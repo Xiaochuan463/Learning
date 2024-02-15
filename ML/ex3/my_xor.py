610171d7f590764c613197ae188901748f397716
Xiaochuan463 <sz7362054@gmail.com>
2024-02-15 17:47:22 +0800

    commoit

--- ML/ex3/my_xor.py
+++ ML/ex3/my_xor.py
@@ -4,6 +4,8 @@ A neural netwoerk to generate Xor
 Now, it is trained to be a 2-4 encoder
 
 and maybe an adder?
+
+It real name is mlp
 """
 
 
@@ -11,11 +13,11 @@ import numpy as np
 from weight import Weight
 
 x = [[0., 0.],
-     [0., 1.], 
+     [0., 1.],
      [1., 0.],
      [1., 1.]]
-y = [[0.,0.], 
-     [0.,1.], 
+y = [[0.,0.],
+     [0.,1.],
      [0.,1.],
      [1.,0.]]
 
@@ -23,27 +25,32 @@ x = np.array(x)
 y = np.array(y)
 
 INPUT_NODES = x.shape[1]
-HIDDEN_NODES = 3
+HIDDEN_NODES_I = 3
+HIDDEN_NODES_II = 5
 OUTPUT_NODES = y.shape[1]
 STUDY_RATE = 0.1
 
-STEP = 10000
+STEP = 50000
 
-ih = Weight(INPUT_NODES, HIDDEN_NODES)
-ho = Weight(HIDDEN_NODES, OUTPUT_NODES)
+ih = Weight(INPUT_NODES, HIDDEN_NODES_I)
+hihii = Weight(HIDDEN_NODES_I, HIDDEN_NODES_II)
+ho = Weight(HIDDEN_NODES_II, OUTPUT_NODES)
 
 for i in range(STEP):
 
     #forward propagation
     hidden_layer = ih.get_output(input_data=x)
-    end = ho.get_output(input_data=ih.output_data)
+    h2 = hihii.get_output(input_data=ih.output_data)
+    end = ho.get_output(input_data=hihii.output_data)
 
     #backward propagation
     ho.get_derivative(next_weight= None, result= y, is_end= True)
-    ih.get_derivative(ho)
+    hihii.get_derivative(ho)
+    ih.get_derivative(hihii)
 
     #update using gradient decent
     ho.update(STUDY_RATE)
+    hihii.update(STUDY_RATE)
     ih.update(STUDY_RATE)
-
+print((end))
 print(np.round(end))
