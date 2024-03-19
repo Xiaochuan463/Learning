f35c0da7ce093cdce1ccabfcccef586771cb29cc
Xiaochuan463 <sz7364052@gmail.com>
2024-03-19 20:51:19 +0800

    commit

--- Algorithm/RBT/inc/rbt.h
+++ Algorithm/RBT/inc/rbt.h
@@ -1,19 +1,37 @@
 #ifndef RBT
 #define RBT
 
-typedef struct rbt_node
+#include<stdlib.h>
+
+typedef struct node node;
+typedef struct rbt rbt;
+
+void rbt_left_rotate(rbt* obj, node*);
+void rbt_right_rotate(rbt* obj, node*);
+void rbt_insert_fixup(rbt* obj,node* insert_node);
+void rbt_insert(rbt* obj, node* insert_node);
+rbt* rbt_new();
+rbt* rbt_free();
+
+typedef struct node
 {
-        struct rbt_node* parent;
-        struct rbt_node* left;
-        struct rbt_node* right;
+        struct node* parent;
+        struct node* left;
+        struct node* right;
 
         int data;
         char color;
-}rbt_node;
+}node;
 
+typedef struct rbt
+{
+        struct node* root;
+        struct node* NIL;
 
-void rbt_left_rotate(rbt_node*);
-void rbt_right_rotate(rbt_node*);
-void rbt_insert_fixup(rbt_node* root, rbt_node* insert_node);
-void rbt_insert(rbt_node* root, rbt_node* insert_node);
+        void (*insert)(rbt* obj, node* insert_node); // Function pointer for insertion
+        void (*insert_fixup)(rbt* obj, node* insert_node); // Function pointer for insertion fixup
+        void (*left_rotate)(rbt* obj, node* node); // Function pointer for left rotation
+        void (*right_rotate)(rbt* obj, node* node);
+}rbt;
+
 #endif
--- Algorithm/RBT/src/rbt.c
+++ Algorithm/RBT/src/rbt.c
@@ -1,7 +1,32 @@
 #include "../inc/rbt.h"
 
-void rbt_left_rotate(rbt_node* x){
-        rbt_node* y;
+rbt* rbt_new(){
+        rbt* tmp = (rbt*)malloc(sizeof(rbt));
+        
+        if(tmp){
+                tmp->insert = rbt_insert;
+                tmp->insert_fixup = rbt_insert_fixup;
+                tmp->left_rotate = rbt_left_rotate;
+                tmp->right_rotate = rbt_right_rotate;
+        
+                tmp->NIL = (node*)malloc(sizeof(node));
+                tmp->root = NULL;
+
+                if(tmp->NIL){
+                        tmp->NIL->color = 'b';
+                        tmp->NIL->left = NULL;
+                        tmp->NIL->right = NULL;
+                        tmp->NIL->parent = NULL;
+                }
+                else{
+                        free(tmp);
+                }
+        }
+        return tmp;
+}
+
+void rbt_left_rotate(rbt* obj, node* x){
+        node* y;
         if(!x || !x->right){
                 return;
         }
@@ -16,8 +41,8 @@ void rbt_left_rotate(rbt_node* x){
         y->left = x;
 }
 
-void rbt_right_rotate(rbt_node* x){
-        rbt_node* y;
+void rbt_right_rotate(rbt* obj,node* x){
+        node* y;
         if(!x || !x->left){
                 return;
         }
@@ -31,31 +56,72 @@ void rbt_right_rotate(rbt_node* x){
         y->right = x;
 }
 
-void rbt_insert_fixup(rbt_node* root, rbt_node* insert_node){
-
-}
-
-void rbt_insert(rbt_node* root, rbt_node* insert_node){
-        rbt_node* x = root, *y = 0;
-        while (x)
-        {
-                y = x;
-                if(x->data > insert_node->data){
-                        x = x->left;
+void rbt_insert_fixup(rbt* obj, node* insert_node){
+        node* tmp;
+        while(insert_node->parent->color == 'r'){
+                if(insert_node->parent == insert_node->parent->parent->left){
+                        tmp = insert_node->parent->parent->right;
+                        if(tmp->color == 'r'){
+                                tmp->color = 'b';
+                                insert_node->color = 'b';
+                                tmp->parent->color = 'r';
+                                insert_node = insert_node->parent->parent;
+                        }
+                        else if(insert_node == insert_node->parent->right){
+                                insert_node = insert_node->parent;
+                                obj->left_rotate(obj, insert_node);
+                        }
+                        insert_node->parent->color = 'b';
+                        insert_node->parent->parent = 'r';
+                        obj->right_rotate(obj, insert_node->parent->parent);
                 }
                 else{
-                        x = x->right;
+                        tmp = insert_node->parent->parent->left;
+                        if(tmp->color == 'r'){
+                                tmp->color = 'b';
+                                insert_node->parent->color = 'b';
+                                insert_node->parent->parent->color = 'r';
+                                insert_node = insert_node->parent->parent;
+                        }
+                        else if(insert_node == insert_node->parent->left){
+                                insert_node = insert_node->parent;
+                                obj->right_rotate(obj, insert_node);
+                        }
+                        insert_node->parent->color = 'b';
+                        insert_node->parent->parent->color = 'r';
+                        obj->left_rotate(obj, insert_node->parent->parent);
                 }
         }
-        insert_node->parent = y;
-        if(y->data > insert_node->data){
-                y->left = insert_node;
+        obj->root->color = 'b';
+}
+
+void rbt_insert(rbt* obj,node* insert_node){
+        node* x = obj->root, *y = obj->NIL;
+        if(!x){
+                obj->root = insert_node;
+                obj->root->parent = obj->NIL;
         }
         else{
-                y->right = insert_node;
+                while (x != obj->NIL)
+                {
+                        y = x;
+                        if(x->data > insert_node->data){
+                                x = x->left;
+                        }
+                        else{
+                                x = x->right;
+                        }
+                }
+                insert_node->parent = y;
+                if(y->data > insert_node->data){
+                        y->left = insert_node;
+                }
+                else{
+                        y->right = insert_node;
+                }
         }
-        insert_node->left = 0;
-        insert_node->right = 0;
-        insert_node->color = 0;
-        rbt_insert_fixup(root, insert_node);
+        insert_node->left = obj->NIL;
+        insert_node->right = obj->NIL;
+        insert_node->color = 'r';
+        rbt_insert_fixup(obj, insert_node);
 }
